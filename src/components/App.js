9f79cdd6b758d2dba7c1a9f48de3c67314800209
koko baghdad <kokobaghdad123@gmail.com>
2022-07-22 01:55:29 +0200

    Finishing the Contacts App!

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,8 @@
 import React from "react";
 import "../css/App.css";
 import ListContacts from "./ListContacts";
+import { useState } from "react";
+
 // {
 //   React.createElement(
 //     "div",
@@ -11,30 +13,103 @@ import ListContacts from "./ListContacts";
 //   );
 // }
 
+var orgContacts = [
+  {
+    id: "karen",
+    name: "xKaren Isgrigg",
+    handle: "karen_isgrigg",
+    avatarURL: "http://localhost:5001/karen.jpg",
+  },
+  {
+    id: "richard",
+    name: "xRichard Kalehoff",
+    handle: "richardkalehoff",
+    avatarURL: "http://localhost:5001/richard.jpg",
+  },
+  {
+    id: "tyler",
+    name: "xTyler McGinnis",
+    handle: "tylermcginnis",
+    avatarURL: "http://localhost:5001/tyler.jpg",
+  },
+  {
+    id: "x",
+    name: "x",
+    handle: "tylermcginnis",
+    avatarURL: "http://localhost:5001/tyler.jpg",
+  },
+  {
+    id: "y",
+    name: "xy",
+    handle: "tylermcginnis",
+    avatarURL: "http://localhost:5001/tyler.jpg",
+  },
+  {
+    id: "tyler1",
+    name: "Tyller McGinnis",
+    handle: "tylermcginnis",
+    avatarURL: "http://localhost:5001/tyler.jpg",
+  },
+];
+
 const App = () => {
-  const contacts = [
-    {
-      id: "karen",
-      name: "Karen Isgrigg",
-      handle: "karen_isgrigg",
-      avatarURL: "http://localhost:5001/karen.jpg",
-    },
-    {
-      id: "richard",
-      name: "Richard Kalehoff",
-      handle: "richardkalehoff",
-      avatarURL: "http://localhost:5001/richard.jpg",
-    },
-    {
-      id: "tyler",
-      name: "Tyler McGinnis",
-      handle: "tylermcginnis",
-      avatarURL: "http://localhost:5001/tyler.jpg",
-    },
-  ];
-  //console.log(contacts[0]);
-  //return ListContacts(contacts);
-  return <ListContacts contacts={contacts} />;
+  const [contacts, setContacts] = useState(orgContacts);
+
+  const [search, setSearch] = useState("");
+  const removeContact = (contact) => (
+    (orgContacts = orgContacts.filter((c) => c.id !== contact.id)),
+    setContacts(orgContacts)
+  );
+
+  const searchContacts = (s) => {
+    setSearch(s);
+    setContacts(
+      orgContacts.filter((contact) =>
+        contact.name.toLowerCase().includes(s.toLowerCase())
+      )
+    );
+  };
+
+  const resetContacts = () => (setSearch(""), setContacts(orgContacts));
+
+  return (
+    <div>
+      <div>
+        <input
+          className="input-field"
+          value={search}
+          type="text"
+          placeholder="Search Contacts"
+          onChange={(event) => searchContacts(event.target.value)}
+        />
+
+        {search.length > 0 && (
+          <div className="info-div">
+            <span>
+              Now showing {contacts.length} of {orgContacts.length}{" "}
+              <button className="button" onClick={resetContacts}>
+                Show All!
+              </button>
+            </span>
+          </div>
+        )}
+      </div>
+
+      <ListContacts
+        contacts={contacts}
+        removeContact={removeContact}
+        className="list-contacts"
+        //removeContact2={removeContact2}
+      />
+      {/* <button
+        onClick={() =>
+          setContacts(contacts.pop(), contacts.length--, console.log(contacts))
+        }
+      >
+        Remove Last Contact
+      </button> */}
+    </div>
+  );
 };
 
 export default App;
--- src/components/ListContacts.js
+++ src/components/ListContacts.js
@@ -1,24 +1,55 @@
-const ListContacts = ({ contacts }) => {
-  //   console.log(contacts);
-  //   console.log({ contacts });
-  return (
-    //<div>Hello World</div>
-    <ul className="contact-list">
-      {contacts.map((contact) => (
-        <li key={contact.id}>
-          <img height={50} src={contact.avatarURL}></img>
-          <ul>
-            <li>
-              <span>{contact.name} </span>
-            </li>
-            <li>
-              <span>{contact.handle}</span>
-            </li>
-          </ul>
-        </li>
-      ))}
-    </ul>
-  );
+import PropTypes from "prop-types";
+
+const ListContacts = ({ contacts, removeContact }) => (
+  <ul className="contact-list">
+    {contacts.map((contact, index) => (
+      <li key={contact.id} className="contact-list-item">
+        <div
+          className="contact-avatar"
+          style={{ backgroundImage: `url(${contact.avatarURL})` }}
+        ></div>
+        <div className="contact-details">
+          <p>{contact.name}</p>
+          <p>@{contact.handle}</p>
+        </div>
+        <button
+          className="contact-remove"
+          onClick={() => {
+            removeContact(contact);
+          }}
+        ></button>
+      </li>
+    ))}
+  </ul>
+);
+
+ListContacts.propTypes = {
+  contacts: PropTypes.array.isRequired,
+  removeContact: PropTypes.func.isRequired,
 };
 
 export default ListContacts;
+
+/*
+<li
+        key={contact.id}
+        style={{
+          borderStyle: "solid",
+          borderColor: "blue",
+          margin: 30,
+          height: 55,
+        }}
+      >
+        <div>
+          <div style={{ float: "left" }}>
+            <img height={50} src={contact.avatarURL}></img>
+          </div>
+          <div style={{ float: "left" }}>
+            {contact.name} <br></br> {contact.id}
+          </div>
+          <div style={{ float: "right", margin: "13px" }}>
+            <button style={{ borderRadius: "40px" }}>X</button>
+          </div>
+        </div>
+      </li>
+*/
